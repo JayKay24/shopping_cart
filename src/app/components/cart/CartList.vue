8b4d9b5d0fb159a5ab536b45f7b555aac2e321a0
James Kinyua <jameskinyua590@gmail.com>
2018-10-18 16:02:17 +0300

    feat(removeItem): configure the removeItem action to call the UPDATE_CART_ITEMS mutation

--- src/app/components/cart/CartList.vue
+++ src/app/components/cart/CartList.vue
@@ -3,7 +3,10 @@
     <div class="cart--header has-text-centered">
       <i class="fa fa-2x fa-shopping-cart"></i>
     </div>
-    <ul>
+    <p v-if="!cartItems.length" class="cart-empty-text has-text-centered">
+      Add some items to the cart!
+    </p>
+    <ul v-if="cartItems.length > 0">
       <li v-for="cartItem in cartItems" :key="cartItem.id" class="cart-item">
         <CartListItem :cartItem="cartItem" />
       </li>
@@ -14,19 +17,19 @@
             {{ cartQuantity }}
           </span>
         </p>
-        <p class="cart-remove-all--text">
+        <p @click="removeAllCartItems" class="cart-remove-all--text">
           <i class="fa fa-trash"></i>Remove all
         </p>
       </div>
     </ul>
-    <button class="button is-primary">
+    <button class="button is-primary" :disabled="!cartItems.length">
       Checkout (<span class="has-text-weight-bold">${{ cartTotal }}</span>)
     </button>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import { mapGetters, mapActions } from 'vuex'
 import CartListItem from './CartListItem'
 
 export default {
@@ -40,6 +43,11 @@ export default {
   },
   components: {
     CartListItem
+  },
+  methods: {
+    ...mapActions([
+      'removeAllCartItems'
+    ])
   },
   created() {
     this.$store.dispatch('getCartItems')
